4518bd3c9ea08b552fad3a455bf72c1b41534ea8
Luiz Carneiro <luuizpaulo@gmail.com>
2020-06-30 23:52:28 +0200

    update tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+  _id: userOneId,
   name: 'Mike',
   email: 'mike@example.com',
-  password: '!Mike11'
+  password: '56what!!',
+  tokens: [{
+    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+  }]
 }
 
 const userTwo = {
@@ -29,13 +36,43 @@ test('should signup a new user', async () => {
 })
 
 test('Should login an existing user', async () => {
-  await request(app).post('/users/login').send({ ...userOne }).expect(200)
-})
-
-test('Should login an existing user', async () => {
-  await request(app).post('/users/login').send({ ...userOne }).expect(200)
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password
+  }).expect(200)
 })
 
 test('Should fail with a non existing user', async () => {
   await request(app).post('/users/login').send({ ...userTwo }).expect('user not found')
+})
+
+test('Should get profile for user', async () => {
+
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should not get profile for user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+test('Should not delete user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect('{"error":"Please authenticate yourself"}')
+})
+
+test('Should delete user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
 })
